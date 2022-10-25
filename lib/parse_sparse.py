55f617dd70953ff1f4ad4be6f202aa28e92b6412
Zhifan Zhu <zhifan.zhu@bristol.ac.uk>
2022-10-25 10:05:20 +0100

    update parse_sparse

--- lib/parse_sparse.py
+++ lib/parse_sparse.py
@@ -1,4 +1,6 @@
 from typing import NamedTuple
+from functools import cached_property
+import os
 import numpy as np
 import sqlite3
 import pandas as pd
@@ -16,27 +18,51 @@ class ColmapDB(NamedTuple):
 
 class SparseProj:
     
-    def __init__(self, proj_root):
-        self.cameras_raw = colmap_utils.read_cameras_binary(f'{proj_root}/sparse/0/cameras.bin')
-        self.points_raw = colmap_utils.read_points3d_binary(f'{proj_root}/sparse/0/points3D.bin')
-        self.images_raw = colmap_utils.read_images_binary(f'{proj_root}/sparse/0/images.bin')
+    def __init__(self, proj_root, model_id=0):
+        prefix = f'{proj_root}/sparse/{model_id}'
+        self.database_path = f'{proj_root}/database.db'
+        self.prefix = prefix
+
+        as_list = lambda x: list(dict(x).values())
+        self.cameras_raw = as_list(colmap_utils.read_cameras_binary(f'{prefix}/cameras.bin'))
+        self.points_raw  = as_list(colmap_utils.read_points3d_binary(f'{prefix}/points3D.bin'))
+        self.images_registered = as_list(colmap_utils.read_images_binary(f'{prefix}/images.bin'))
 
         # Build point cloud
         pts, clr = [], []
-        for k, v in self.points_raw.items():
+        for v in self.points_raw:
             xyz, rgb = v.xyz, v.rgb
             pts.append(xyz)
             clr.append(rgb)
         self.pcd_pts = np.stack(pts, 0)
         self.pcd_clr = np.stack(clr, 0)
-
-        self.database_path = f'{proj_root}/database.db'
     
-    @property
-    def database(self):
+    @cached_property
+    def summary(self):
+        num_models = len(os.listdir(self.prefix))
+        num_observes = np.mean(
+            [sum(v.point3D_ids > 0) for v in self.images_registered])
 
-        if hasattr(self, '_database') and self._database is not None:
-            return self._database
+        track_lens, errors = [], []
+        for v in self.points_raw:
+            track_lens.append(len(v.image_ids))
+            errors.append(v.error)
+
+        r = {
+            'reg_images': len(self.images_registered),
+            'reproj_error': np.mean(errors),
+            'track_length': np.mean(track_lens),
+            'obs_per_img': num_observes,
+            'num_models': num_models,
+        }
+        return r
+    
+    def print_summary(self):
+        for k, v in self.summary.items():
+            print(f'{k}\t=\t{v:.3f}')
+    
+    @cached_property
+    def database(self):
 
         def _make_df(fields, results):
             data = dict()
@@ -83,7 +109,7 @@ class SparseProj:
                 #  'qvec', 'tvec'],
                 results=cur.fetchall())
         
-        self._database = ColmapDB(**dict(
+        return ColmapDB(**dict(
             cameras=cameras,
             descriptors=descriptors,
             images=images,
@@ -91,7 +117,6 @@ class SparseProj:
             matches=matches,
             two_view_geometries=two_view_geometries
             ))
-        return self._database
 
     @property
     def pcd(self):
