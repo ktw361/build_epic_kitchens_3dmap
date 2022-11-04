cc1402b203c693a8ccaf5bad2fd73272763eec58
Zhifan Zhu <zhifan.zhu@bristol.ac.uk>
2022-11-04 11:09:55 +0000

    Update path

--- lib/constants.py
+++ lib/constants.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+from hydra.utils import to_absolute_path
 
-DATA = Path('./visor_data')
+DATA = Path(to_absolute_path('./visor_data'))
 
 IMG_ROOT = DATA/'sparse_images'  # (1920 x 1080)
 IMG_MED_ROOT = DATA/'sparse_images_medium'  # (854 x 480)
@@ -9,9 +10,9 @@ MASK_ROOT = DATA/'sparse_masks'  # Note: colored
 BIN_MASK_ROOT = DATA/'sparse_binary_masks'
 BIN_MASK_MED_ROOT = DATA/'sparse_binary_masks_medium'
 
-VOCAB_32K = Path('./vocab_bins/vocab_tree_flickr100K_words32K.bin')
-VOCAB_256K = Path('./vocab_bins/vocab_tree_flickr100K_words256K.bin')
-VOCAB_1M = Path('./vocab_bins/vocab_tree_flickr100K_words1M.bin')
+VOCAB_32K   = Path(to_absolute_path('./vocab_bins/vocab_tree_flickr100K_words32K.bin'))
+VOCAB_256K  = Path(to_absolute_path('./vocab_bins/vocab_tree_flickr100K_words256K.bin'))
+VOCAB_1M    = Path(to_absolute_path('./vocab_bins/vocab_tree_flickr100K_words1M.bin'))
 
 IMAGEREADER = 'ImageReader'
 SIFTEXTRACTION = 'SiftExtraction'
